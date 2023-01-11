e7308ecfa137134f208fab50250aaaa3a949be79
Hodaka Nakamura <htoytoytok@gmail.com>
2023-01-11 13:21:27 -0800

    enemy

--- Assets/scripts/enemy.cs
+++ Assets/scripts/enemy.cs
@@ -7,10 +7,63 @@ public class enemy : MonoBehaviour
 {
     NavMeshAgent agent;
 
+    bool patrol = false;
+
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        agent.SetDestination(new Vector3(0f, 0f, 0f));
+
+        ScanAndMove();
+    }
+
+    void ScanAndMove()
+    {
+        GameObject[] towers = GameObject.FindGameObjectsWithTag("Tower");
+
+        if (towers.Length != 0)
+        {
+            patrol = false;
+
+            float closestDist = Mathf.Infinity;
+            Vector3 closestTowerPos = new Vector3();
+
+            foreach (GameObject tower in towers)
+            {
+                float dist = Vector3.SqrMagnitude(transform.position - tower.transform.position);
+
+                if (closestDist > dist)
+                {
+                    closestDist = dist;
+                    closestTowerPos = tower.transform.position;
+                }
+            }
+
+            agent.SetDestination(closestTowerPos);
+        } else
+        {
+            patrol = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (patrol)
+        {
+            ScanAndMove();
+
+            if (!agent.pathPending && agent.remainingDistance < 0.5f)
+                Patrol();
+        }
+
+        if (!patrol && !agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            ScanAndMove();
+        }
+    }
+
+    void Patrol()
+    {
+        agent.SetDestination(new Vector3(Random.Range(-8f, 8f), 1f, Random.Range(-8f, 8f)));
     }
 }
