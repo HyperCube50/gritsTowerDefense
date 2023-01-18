cc055faf7554e02d2e893a016eed9a52ce97bdc3
Hodaka Nakamura <htoytoytok@gmail.com>
2023-01-18 13:21:18 -0800

    stuff

---
+++ Assets/scripts/enemySpawner.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnEnemy", 0f, 5f);
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemy, new Vector3(8f, 1f, 0f), Quaternion.identity);
+    }
+}
