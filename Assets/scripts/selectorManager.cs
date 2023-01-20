b93d753ec17d0debb3a610dcb7a514e6f32e59f2
Hodak Nak <htoytoytok@gmail.com>
2023-01-20 11:41:28 -0800

    UI stuff

---
+++ Assets/scripts/selectorManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class selectorManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] buttons;
+
+    public enum TowerType
+    {
+        OVERHEAT,
+        MONKE
+    }
+
+    public TowerType currentType = TowerType.OVERHEAT;
+
+    public void updateHighlights()
+    {
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].GetComponent<Outline>().enabled = false;
+        }
+
+        buttons[(int)currentType].GetComponent<Outline>().enabled = true;
+    }
+}
---
+++ Assets/scripts/towerButton.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class towerButton : MonoBehaviour
+{
+    [SerializeField]
+    selectorManager manager;
+
+    [SerializeField]
+    selectorManager.TowerType type;
+
+    public void onClick()
+    {
+        manager.currentType = type;
+        manager.updateHighlights();
+    }
+}
